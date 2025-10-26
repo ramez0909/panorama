5e5636712feeb3fd803c04774feca3150c47d814
ramez0909 <113442372+ramez0909@users.noreply.github.com>
2025-10-26 18:57:58 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,126 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-whatsapp-image-2025-10-25-at-103822_b941a087",
+      "name": "WhatsApp Image 2025-10-25 at 10.38.22_b941a087",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "yaw": 0.9531827476253909,
+        "pitch": 0.3424499478120886,
+        "fov": 1.2912494743184422
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2536051821904515,
+          "pitch": 0.5800521506800251,
+          "rotation": 0,
+          "target": "1-whatsapp-image-2025-10-25-at-105245_78fb8331---whatsapp-image-2025-10-25-at-105245_78fb8331"
+        },
+        {
+          "yaw": -1.5101777691539002,
+          "pitch": 0.227830209702665,
+          "rotation": 0,
+          "target": "2-whatsapp-image-2025-10-25-at-105956_4f797f6d"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-whatsapp-image-2025-10-25-at-105245_78fb8331---whatsapp-image-2025-10-25-at-105245_78fb8331",
+      "name": "WhatsApp Image 2025-10-25 at 10.52.45_78fb8331 - WhatsApp Image 2025-10-25 at 10.52.45_78fb8331",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 280,
+      "initialViewParameters": {
+        "yaw": -2.5772953749754706,
+        "pitch": 0,
+        "fov": 1.2912494743184422
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2422381738553376,
+          "pitch": 0.04020451248661239,
+          "rotation": 0,
+          "target": "0-whatsapp-image-2025-10-25-at-103822_b941a087"
+        },
+        {
+          "yaw": 2.069610148476352,
+          "pitch": 0.17566340780851242,
+          "rotation": 0,
+          "target": "2-whatsapp-image-2025-10-25-at-105956_4f797f6d"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.0052376783999825705,
+          "pitch": 0,
+          "title": "2 sp",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-whatsapp-image-2025-10-25-at-105956_4f797f6d",
+      "name": "WhatsApp Image 2025-10-25 at 10.59.56_4f797f6d",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "yaw": 0.42587330413470426,
+        "pitch": 0.15010735084896432,
+        "fov": 1.2912494743184422
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2853325607182953,
+          "pitch": 0.10558500603734444,
+          "rotation": 0,
+          "target": "0-whatsapp-image-2025-10-25-at-103822_b941a087"
+        },
+        {
+          "yaw": 1.7009131694969453,
+          "pitch": 0.07700555274147902,
+          "rotation": 0,
+          "target": "1-whatsapp-image-2025-10-25-at-105245_78fb8331---whatsapp-image-2025-10-25-at-105245_78fb8331"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "qtvr",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
